6cb8773af1482b69c4e711bf9479497f8cb44d47
kenokim <59631871+kenokim@users.noreply.github.com>
2022-02-04 16:03:18 +0900

    commit

---
+++ src/test/java/keno/swaggerTest/SimpleControllerTest.java
@@ -0,0 +1,10 @@
+package keno.swaggerTest;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class SimpleControllerTest {
+
+}
